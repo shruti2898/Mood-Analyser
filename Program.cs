951f305620d9326968e0cb22f4f5453ef352f07c
Shruti <shrutisablaniya93@gmail.com>
2021-10-11 20:05:10 +0530

    Mood Analyser Problem

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+        }
+    }
+}
