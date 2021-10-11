86c74a34a6a7d8ea86c6ed10a36980b54e90d590
Shruti <shrutisablaniya93@gmail.com>
2021-10-11 20:12:42 +0530

    Mood Analyser Demo

---
+++ MoodAnalyserDemo/MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserDemo
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMood()
+        {
+            //"null"==""
+            // string s = null;
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+
+}
